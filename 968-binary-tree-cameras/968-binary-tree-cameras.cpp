269d81e65068875469a481ee22ca2758b08c0434
YunGoon <dsyun96@gmail.com>
2022-06-17 20:11:03 +0900

    Time: 53 ms (5.10%), Space: 25.8 MB (11.52%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -14,28 +14,15 @@ public:
     unordered_map<TreeNode*, int> D[2];
     
     int minCameraCover(TreeNode* root, bool check = false) {
-        if (D[check].find(root) != D[check].end()) return D[check][root];
         if (root == nullptr) return 0;
+        if (D[check].find(root) != D[check].end()) return D[check][root];
         
         int ret = 0x3f3f3f3f;
         if (check) ret = min(ret, minCameraCover(root->left, 0) + minCameraCover(root->right, 0));
         
-        if (root->left && root->right) {
-            ret = min(ret, minCameraCover(root->left, 1) + minCameraCover(root->right, 1) + 1);
-            ret = min(ret, minCameraCover(root->left->left, 1) + minCameraCover(root->left->right, 1) + minCameraCover(root->right, 0) + 1);
-            ret = min(ret, minCameraCover(root->right->left, 1) + minCameraCover(root->right->right, 1) + minCameraCover(root->left, 0) + 1);
-        }
-        else if (root->left) {
-            ret = min(ret, minCameraCover(root->left, 1) + 1);
-            ret = min(ret, minCameraCover(root->left->left, 1) + minCameraCover(root->left->right, 1) + 1);
-        }
-        else if (root->right) {
-            ret = min(ret, minCameraCover(root->right, 1) + 1);
-            ret = min(ret, minCameraCover(root->right->left, 1) + minCameraCover(root->right->right, 1) + 1);
-        }
-        else {
-            ret = !check;
-        }
+        ret = min(ret, minCameraCover(root->left, 1) + minCameraCover(root->right, 1) + 1);
+        if (root->left) ret = min(ret, minCameraCover(root->left->left, 1) + minCameraCover(root->left->right, 1) + minCameraCover(root->right, 0) + 1);
+        if (root->right) ret = min(ret, minCameraCover(root->right->left, 1) + minCameraCover(root->right->right, 1) + minCameraCover(root->left, 0) + 1);
         
         return D[check][root] = ret;
     }
