d00b4211f3108d1db880d56e397598d054299a55
YunGoon <dsyun96@gmail.com>
2022-06-20 20:21:26 +0900

    Time: 74 ms (70.79%), Space: 13.8 MB (96.04%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -4,12 +4,12 @@ public:
         for (auto& word : words) reverse(begin(word), end(word));
         sort(begin(words), end(words));
         
-        int ret = 0;
+        int ret = words.back().size() + 1;
         for (int i = 1; i < words.size(); ++i) {
             int len = min(words[i - 1].size(), words[i].size());
             if (words[i - 1].substr(0, len) != words[i].substr(0, len)) ret += words[i - 1].size() + 1;
         }
         
-        return ret + words.back().size() + 1;
+        return ret;
     }
 };
