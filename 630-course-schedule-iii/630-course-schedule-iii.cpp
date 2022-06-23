4d89d2e9e9eebf858cfe6e2d0dc957ab64b1135c
YunGoon <dsyun96@gmail.com>
2022-06-23 23:44:04 +0900

    Time: 515 ms (22.13%), Space: 56.3 MB (58.52%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        sort(courses.begin(), courses.end(), [](auto& a, auto& b) { return a[1] < b[1]; });
+        
+        priority_queue<int> PQ;
+        int cnt = 0, sum = 0;
+        for (auto& course : courses) {
+            if (sum + course[0] <= course[1]) {
+                sum += course[0];
+                PQ.emplace(course[0]);
+                ++cnt;
+            }
+            else if (!PQ.empty() && PQ.top() > course[0]) {
+                sum -= PQ.top(); PQ.pop();
+                sum += course[0];
+                PQ.emplace(course[0]);
+            }
+        }
+        
+        return cnt;
+    }
+};
