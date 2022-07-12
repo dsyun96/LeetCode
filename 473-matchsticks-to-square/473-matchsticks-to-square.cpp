c3b5c0f332c9f4bb98a77483620b5d89fe00b096
YunGoon <dsyun96@gmail.com>
2022-07-12 21:37:04 +0900

    Time: 385 ms (49.76%), Space: 10.5 MB (29.20%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -6,6 +6,13 @@ public:
         if (sum % 4) return false;
         sum /= 4;
         
+        vector<int> valid;
+        for (int i = (1 << n) - 1; i; i = i - 1 & (1 << n) - 1) {
+            int s = 0;
+            for (int j = 0; j < n; ++j) if (i & 1 << j) s += matchsticks[j];
+            if (s == sum) valid.push_back(i);
+        }
+        
         char dp[3][1 << 15]; memset(dp, -1, sizeof dp);
         auto go = [&](auto& self, int cnt, int state) -> char {
             if (cnt == 3) return 1;
@@ -14,16 +21,11 @@ public:
             if (~ret) return ret;
             
             ret = 0;
-            for (int bit = state; bit; bit = bit - 1 & state) {
-                int s = 0;
-                for (int j = 0; j < n; ++j) if (bit & 1 << j) s += matchsticks[j];
-                
-                if (s == sum && self(self, cnt + 1, state ^ bit)) return 1;
-            }
+            for (int it : valid) if (!(it & state) && self(self, cnt + 1, state | it)) return 1;
             
             return ret;
         };
         
-        return go(go, 0, (1 << n) - 1);
+        return go(go, 0, 0);
     }
 };
