a14e1abeb61dfa99397ecae147cfa25c10ce9997
YunGoon <dsyun96@gmail.com>
2022-08-07 14:51:58 +0900

    Time: 6 ms (87.38%), Space: 6 MB (85.05%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int countVowelPermutation(int n) {
-        int dp[20000][5];
+        int dp[2][5];
         for (int i = 0; i < 5; ++i) dp[0][i] = 1;
         
         const int MOD = 1e9 + 7;
+        int idx = 0;
         for (int i = 1; i < n; ++i) {
-            dp[i][0] = (0ll + dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][4]) % MOD;
-            dp[i][1] = (0ll + dp[i - 1][0] + dp[i - 1][2]) % MOD;
-            dp[i][2] = (0ll + dp[i - 1][1] + dp[i - 1][3]) % MOD;
-            dp[i][3] = (0ll + dp[i - 1][2]) % MOD;
-            dp[i][4] = (0ll + dp[i - 1][2] + dp[i - 1][3]) % MOD;
+            idx ^= 1;
+            dp[idx][0] = (0ll + dp[idx ^ 1][1] + dp[idx ^ 1][2] + dp[idx ^ 1][4]) % MOD;
+            dp[idx][1] = (0ll + dp[idx ^ 1][0] + dp[idx ^ 1][2]) % MOD;
+            dp[idx][2] = (0ll + dp[idx ^ 1][1] + dp[idx ^ 1][3]) % MOD;
+            dp[idx][3] = (0ll + dp[idx ^ 1][2]) % MOD;
+            dp[idx][4] = (0ll + dp[idx ^ 1][2] + dp[idx ^ 1][3]) % MOD;
         }
         
-        return accumulate(dp[n - 1], dp[n - 1] + 5, 0ll) % MOD;
+        return accumulate(dp[idx], dp[idx] + 5, 0ll) % MOD;
     }
 };
